0cdac49ffc7a2749712017c929de58d52309cf56
gitHero14 <160379612+gitHero14@users.noreply.github.com>
2024-02-25 16:18:51 +0300

    First commit
    My first assignment in Plp to get users input

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('Type your name: ')
+age = input('Enter your age: ')
+location = input('Enter your location: ')
+#This code helps me get users' information
+print("Hello [],you are [age]years old and live in[location]")
